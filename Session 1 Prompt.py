24f91be2fdd5e93245d79bb60f751e93caaa8eb2
JGorosh <jgorosh@ucsc.edu>
2024-10-22 11:31:52 -0700

    Session 1 prompt for Coding Journal 1

---
+++ Session 1 Prompt.py
@@ -0,0 +1,10 @@
+def main():
+    full_name = "Jonathan Gorosh"
+    favorite_movie = "The Dark Knight"
+    favorite_food = "eggs benedict"
+
+    print("My name is", full_name + ".")
+    print("My favorite movie is", favorite_movie + ".")
+    print("My favorite food is", favorite_food + ".")
+    
+main()
