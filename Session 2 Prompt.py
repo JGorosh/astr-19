0512735fd4588799364f362a044af605a02f5a08
JGorosh <jgorosh@ucsc.edu>
2024-10-22 11:39:11 -0700

    Session 2 Prompt for Coding Journal 1

---
+++ Session 2 Prompt.py
@@ -0,0 +1,20 @@
+def main():
+    
+    float_num1 = 5.7
+    float_num2 = 3.3
+    sum_result = float_num1 + float_num2
+    print(f"Sum of {float_num1} + {float_num2} is {sum_result}, and its type is {type(sum_result)}.")
+
+    
+    int_num1 = 15
+    int_num2 = 8
+    diff_result = int_num1 - int_num2
+    print(f"Difference between {int_num1} - {int_num2} is {diff_result}, and its type is {type(diff_result)}.")
+
+    
+    float_num3 = 2.5
+    int_num3 = 4
+    prod_result = float_num3 * int_num3
+    print(f"Product of {float_num3} x {int_num3} is {prod_result}, and its type is {type(prod_result)}.")
+
+main()
